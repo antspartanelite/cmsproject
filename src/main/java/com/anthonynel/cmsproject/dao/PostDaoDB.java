766cb1aab92138a3ede8eb2ddeedd01f21fe8865
Anthony Nel <antspartanelite@gmail.com>
2021-11-30 17:10:04 +0000

    Fixed issue now runs

--- src/main/java/com/anthonynel/cmsproject/dao/PostDaoDB.java
+++ src/main/java/com/anthonynel/cmsproject/dao/PostDaoDB.java
@@ -4,11 +4,13 @@ import com.anthonynel.cmsproject.dto.Blogpost;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+@Repository
 public class PostDaoDB implements PostDao{
 
     @Autowired
