9ad9b0c42fbb6cdeb8a2e8cc5a6ccaba65ffbbc5
Anthony Nel <antspartanelite@gmail.com>
2021-12-01 04:01:55 +0000

    can post and view posts properly

--- src/main/java/com/anthonynel/cmsproject/controllers/PostingController.java
+++ src/main/java/com/anthonynel/cmsproject/controllers/PostingController.java
@@ -1,20 +1,35 @@
 package com.anthonynel.cmsproject.controllers;
 
+import com.anthonynel.cmsproject.dao.PostDao;
 import com.anthonynel.cmsproject.dto.Blogpost;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class PostingController {
+
+    @Autowired
+    PostDao postDao;
+
     @GetMapping("posterpage")
-    public String displayLogin(){
+    public String displayLogin(Model model){
+        Blogpost blogpost = new Blogpost();
+        model.addAttribute("blogpost", blogpost);
+        List<Blogpost> blogposts = postDao.getPosts();
+        model.addAttribute("blogposts", blogposts);
         return "posterpage";
     }
 
     @PostMapping("createPost")
     public String createPost(Blogpost blogpost){
-        System.out.println(blogpost.getPost());
-        return "posterpage";
+        blogpost.setApproved(false);
+        blogpost.setPostTime("2013-03-03 19:22:11");
+        postDao.addPost(blogpost);
+        return "redirect:/posterpage";
     }
 }
